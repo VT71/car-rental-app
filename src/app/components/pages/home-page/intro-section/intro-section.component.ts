21e2e07715da13b0f2cdf5b82d577e4fc684401b
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-19 22:55:15 +0100

    Customised styling for select input by material

--- src/app/components/pages/home-page/intro-section/intro-section.component.ts
+++ src/app/components/pages/home-page/intro-section/intro-section.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-intro-section',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatInputModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './intro-section.component.html',
-  styleUrl: './intro-section.component.css'
+  styleUrl: './intro-section.component.css',
 })
-export class IntroSectionComponent {
-
-}
+export class IntroSectionComponent {}
