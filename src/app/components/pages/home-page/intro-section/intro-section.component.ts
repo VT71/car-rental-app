9b8d3fc80a5b16925db17cbb7e40d44d64adf873
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-21 21:04:20 +0100

    Added pick-up date input

--- src/app/components/pages/home-page/intro-section/intro-section.component.ts
+++ src/app/components/pages/home-page/intro-section/intro-section.component.ts
@@ -3,11 +3,26 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-intro-section',
   standalone: true,
-  imports: [MatIconModule, MatInputModule, MatFormFieldModule, MatSelectModule],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+    provideNativeDateAdapter(),
+  ],
+  imports: [
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatButtonModule,
+  ],
   templateUrl: './intro-section.component.html',
   styleUrl: './intro-section.component.css',
 })
