6ad129f3c74631372c0e35df66f529c5c4c4c6d7
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-24 12:25:42 +0100

    Attached fleet component to home page

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { IntroSectionComponent } from './intro-section/intro-section.component';
+import { FleetComponent } from './fleet/fleet.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [IntroSectionComponent],
+  imports: [IntroSectionComponent, FleetComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
