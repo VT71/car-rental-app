cf2fc691dba1b77dbb60c97b1f2c1e059fe36aa1
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-24 21:18:53 +0100

    Added the reviews section + styling

--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { IntroSectionComponent } from './intro-section/intro-section.component';
 import { FleetComponent } from './fleet/fleet.component';
+import { TestimonialsSectionComponent } from './testimonials-section/testimonials-section.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [IntroSectionComponent, FleetComponent],
+  imports: [
+    IntroSectionComponent,
+    FleetComponent,
+    TestimonialsSectionComponent,
+  ],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
 })
---
+++ src/app/components/pages/home-page/testimonials-section/testimonials-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-testimonials-section',
+  standalone: true,
+  imports: [MatIcon],
+  templateUrl: './testimonials-section.component.html',
+  styleUrl: './testimonials-section.component.css'
+})
+export class TestimonialsSectionComponent {
+
+}
