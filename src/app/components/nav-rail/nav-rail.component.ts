df3caa1bb664756ac377d6f697e47469dd39aefb
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-25 15:51:05 +0100

    Added anchor scrolling and connected Home nav-rail link

--- src/app/components/nav-rail/nav-rail.component.ts
+++ src/app/components/nav-rail/nav-rail.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-rail',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-rail.component.html',
   styleUrl: './nav-rail.component.css'
 })
