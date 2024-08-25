f6847b9f7fd5158c46d9e38cea16b32f254b3c53
VT71 <56217276+VT71@users.noreply.github.com>
2024-08-25 21:05:37 +0100

    Created a footer component and added it to the global layout template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavRailComponent } from './components/nav-rail/nav-rail.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavRailComponent],
+  imports: [RouterOutlet, NavRailComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
