4c67fa534bcea32406c81d4d27ecb5c71443bf80
VT71 <56217276+VT71@users.noreply.github.com>
2024-09-29 20:41:03 +0100

    Added validation and dynamic validators for the search form

--- src/app/components/search-form/search-form.component.ts
+++ src/app/components/search-form/search-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, Input } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -8,9 +8,19 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { LocationsApiService } from '../../services/locations-api-service/locations-api.service';
-import { combineLatest, forkJoin, from, Observable } from 'rxjs';
+import { combineLatest, forkJoin, from, Observable, Subscription } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { RentalLocation } from '../../interfaces/rental-location';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-search-form',
@@ -27,12 +37,15 @@ import { RentalLocation } from '../../interfaces/rental-location';
     MatDatepickerModule,
     MatButtonModule,
     AsyncPipe,
+    ReactiveFormsModule,
   ],
   templateUrl: './search-form.component.html',
   styleUrl: './search-form.component.css',
 })
-export class SearchFormComponent {
+export class SearchFormComponent implements OnInit, OnDestroy {
   @Input() showFormTitle!: boolean;
+
+  private subscriptions: Subscription[] = [];
 
   private locationsApiService = inject(LocationsApiService);
 
@@ -44,4 +57,51 @@ export class SearchFormComponent {
     this.$pickUpLocations,
     this.$dropOffLocations,
   ]);
+  public locationsData!: RentalLocation[][];
+
+  private formBuilder = inject(FormBuilder);
+  public searchForm = this.formBuilder.group({
+    pickUpLocation: ['', [Validators.required]],
+    dropOffLocation: ['', [Validators.required]],
+    pickUpDateTime: ['', [Validators.required]],
+    dropOffDateTime: ['', [Validators.required]],
+  });
+
+  onSubmit() {
+    // console.log('FORM DATA: ' + JSON.stringify(this.searchForm.getRawValue()));
+  }
+
+  private locationValidator(locations: RentalLocation[]): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      for (let location of locations) {
+        if (location.id == control.value?.id) {
+          return null;
+        }
+      }
+      return { invalidLocation: { value: control.value } };
+    };
+  }
+
+  ngOnInit() {
+    this.subscriptions.push(
+      this.$locationsData.subscribe((data) => {
+        this.locationsData = data;
+        this.searchForm
+          .get('pickUpLocation')
+          ?.addValidators(this.locationValidator(this.locationsData[0]));
+
+        this.searchForm
+          .get('dropOffLocation')
+          ?.addValidators(this.locationValidator(this.locationsData[1]));
+
+        this.searchForm.reset();
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    for (let subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
 }
