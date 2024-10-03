d2122dc750023365826ff4f7b11a11983881f789
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-03 18:28:02 +0100

    Home page top choices section now gets the car information and passes it to the child fleet component

--- src/app/components/pages/home-page/top-choices-section/top-choices-section.component.ts
+++ src/app/components/pages/home-page/top-choices-section/top-choices-section.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FleetComponent } from '../../../fleet/fleet.component';
+import { CarsApiService } from '../../../../services/cars-api-service/cars-api.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-top-choices-section',
   standalone: true,
-  imports: [FleetComponent],
+  imports: [FleetComponent, AsyncPipe],
   templateUrl: './top-choices-section.component.html',
   styleUrl: './top-choices-section.component.css',
 })
-export class TopChoicesSectionComponent {}
+export class TopChoicesSectionComponent {
+  private carsApiService = inject(CarsApiService);
+
+  public $cars = this.carsApiService.getAll();
+}
