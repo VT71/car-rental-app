735e04c462e37cf6800718f34034fcba04650fc8
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-05 18:58:03 +0100

    Connected cars api call when query parameters change, based on the search form inputs

--- src/app/components/pages/fleet-page/search-fleet/search-fleet.component.ts
+++ src/app/components/pages/fleet-page/search-fleet/search-fleet.component.ts
@@ -1,14 +1,59 @@
-import { Component } from '@angular/core';
-import { SearchFormComponent } from "../../../search-form/search-form.component";
+import { Component, inject, OnInit } from '@angular/core';
+import { SearchFormComponent } from '../../../search-form/search-form.component';
 import { FleetComponent } from '../../../fleet/fleet.component';
+import { CarsApiService } from '../../../../services/cars-api-service/cars-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { concatMap, Observable, of } from 'rxjs';
+import { Car } from '../../../../interfaces/car';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-search-fleet',
   standalone: true,
-  imports: [SearchFormComponent, FleetComponent],
+  imports: [SearchFormComponent, FleetComponent, AsyncPipe],
   templateUrl: './search-fleet.component.html',
-  styleUrl: './search-fleet.component.css'
+  styleUrl: './search-fleet.component.css',
 })
-export class SearchFleetComponent {
+export class SearchFleetComponent implements OnInit {
+  private carsApiService = inject(CarsApiService);
+  private activatedRoute = inject(ActivatedRoute);
 
+  public $carsData!: Observable<Car[]>;
+
+  ngOnInit(): void {
+    this.$carsData = this.activatedRoute.queryParamMap.pipe(
+      concatMap((queryMap) => {
+        let pickUpLocationIdStr = queryMap.get('pickUpLocation');
+        let dropOffLocationIdStr = queryMap.get('dropOffLocation');
+        let pickUpDateTimeIdStr = queryMap.get('pickUpDateTime');
+        let dropOffDateTimeStr = queryMap.get('dropOffDateTime');
+        if (
+          pickUpLocationIdStr &&
+          dropOffLocationIdStr &&
+          pickUpDateTimeIdStr &&
+          dropOffDateTimeStr
+        ) {
+          let pickUpLocationId = parseInt(pickUpLocationIdStr, 10);
+          let dropOffLocationId = parseInt(dropOffLocationIdStr, 10);
+          let pickUpDateTime = Date.parse(pickUpDateTimeIdStr);
+          let dropOffDateTime = Date.parse(dropOffDateTimeStr);
+
+          if (
+            pickUpLocationId &&
+            dropOffLocationId &&
+            pickUpDateTime &&
+            dropOffDateTime
+          ) {
+            return this.carsApiService.getAvailableCars(
+              pickUpLocationId,
+              dropOffLocationId,
+              new Date(pickUpDateTime).toISOString(),
+              new Date(dropOffDateTime).toISOString()
+            );
+          }
+        }
+        return of([]);
+      })
+    );
+  }
 }
