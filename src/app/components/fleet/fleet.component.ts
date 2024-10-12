71e74f0c6ec5fb597340daedd9534c0a136f4ad2
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-12 18:05:57 +0100

    Add type property for the fleet componenet and removed book now buttons from the top choices section cars

--- src/app/components/fleet/fleet.component.ts
+++ src/app/components/fleet/fleet.component.ts
@@ -15,6 +15,7 @@ import { RouterLink } from '@angular/router';
 })
 export class FleetComponent {
   @Input() cars!: Car[];
+  @Input() type!: string; 
 
   public currencySign = environment.currency.sign;
 }
