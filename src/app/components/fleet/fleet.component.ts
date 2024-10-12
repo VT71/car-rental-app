5516c5f62a57952facc6b5f27f813134bef8a072
VT71 <56217276+VT71@users.noreply.github.com>
2024-10-12 19:08:14 +0100

    Booking info will now be transmitted to the booking page via query parameters after a search operation

--- src/app/components/fleet/fleet.component.ts
+++ src/app/components/fleet/fleet.component.ts
@@ -1,21 +1,40 @@
 import { AsyncPipe } from '@angular/common';
-import { Component, inject, Input, OnInit } from '@angular/core';
+import { Component, inject, Input, OnDestroy, OnInit } from '@angular/core';
 import { CarsApiService } from '../../services/cars-api-service/cars-api.service';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { Car } from '../../interfaces/car';
 import { environment } from '../../../environments/environment.development';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 
 @Component({
-  selector: 'app-fleet',
-  standalone: true,
-  imports: [AsyncPipe, RouterLink],
-  templateUrl: './fleet.component.html',
-  styleUrl: './fleet.component.css',
+    selector: 'app-fleet',
+    standalone: true,
+    imports: [AsyncPipe, RouterLink],
+    templateUrl: './fleet.component.html',
+    styleUrl: './fleet.component.css',
 })
-export class FleetComponent {
-  @Input() cars!: Car[];
-  @Input() type!: string; 
+export class FleetComponent implements OnDestroy {
+    @Input() cars!: Car[];
+    @Input() type!: string;
 
-  public currencySign = environment.currency.sign;
+    private route = inject(ActivatedRoute);
+    private router = inject(Router);
+    private subscriptions: Subscription[] = [];
+
+    public currencySign = environment.currency.sign;
+
+    bookHandler(carId: number) {
+        this.subscriptions.push(this.route.queryParamMap.subscribe(queryParams => {
+            if (queryParams.get("pickUpLocation") && queryParams.get("dropOffLocation") && queryParams.get("pickUpDateTime") && queryParams.get("dropOffDateTime")) {
+                this.router.navigateByUrl(`/book/${carId}?pickUpLocation=${queryParams.get("pickUpLocation")}&dropOffLocation=${queryParams.get("dropOffLocation")}&pickUpDateTime=${queryParams.get("pickUpDateTime")}&dropOffDateTime=${queryParams.get("dropOffDateTime")}`);
+            }
+        }));
+    }
+
+    ngOnDestroy(): void {
+        for (let subscription of this.subscriptions) {
+            subscription.unsubscribe();
+        }
+    }
+
 }
